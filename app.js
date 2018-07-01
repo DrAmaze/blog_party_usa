68e94941077df57ee398e7e5cbbaa452a26f2488
jacob <jbutler8@terpmail.umd.edu>
2018-06-30 19:54:22 -0700

    Add blog seed data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: './variables.env' });
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 const morgan = require('morgan');
 const app = express();
 
@@ -10,7 +11,14 @@ app.set('view engine', 'pug');
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'styles')));
 
-app.get('/', (req, res) => res.render('index', {}));
+const blogFile = fs.readFileSync('./seeds/db.json', 'utf-8');
+const blogArray = JSON.parse(blogFile);
+
+app.get('/', (req, res) =>
+  res.render('index', {
+    blogs: blogArray
+  })
+);
 
 app.listen(process.env.PORT, () =>
   console.log(`i am listening on ${process.env.PORT}`)
