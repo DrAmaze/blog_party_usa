4f79bf787c81f663fd0f30584f746cd0f3a7be8d
jacob <jbutler8@terpmail.umd.edu>
2018-06-30 19:08:58 -0700

    install PUG dependencies

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require('dotenv').config({ path: './variables.env' });
 let express = require('express');
 const app = express();
-console.log(process.env.PORT)
-app.get('/', (req, res) => res.end('hey what is good brahhhhhlean?'));
+
+app.set('views', 'views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => res.render('hello', {title: 'Title'}));
 
 app.listen(process.env.PORT, () =>
   console.log(`i am listening on ${process.env.PORT}`)
